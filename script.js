a131e71973c208305966fa6c1096435aef6fdd7e
darweshmasizo-lgtm <darweshmasizo@gmail.com>
2026-02-02 12:20:51 +0000

    add const navbarOffsetTop

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
 
 window.addEventListener("scroll",() =>{
 
-    if( window.pageYOffset >= navbar.offsetTop)
+    if( window.pageYOffset >= navbar.offsetTop){
+     navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
         
 })
